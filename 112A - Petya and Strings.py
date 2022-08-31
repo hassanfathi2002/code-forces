d8ff2b85c18c4aaeb7176268356bc06ca18e0bd8
hassanfathi2002 <88568710+hassanfathi2002@users.noreply.github.com>
2022-08-31 11:51:07 +0200

    Create 112A - Petya and Strings.py

---
+++ 112A - Petya and Strings.py
@@ -0,0 +1,9 @@
+#170368431	Aug/31/2022 11:49UTC+2	hassanfathi2002	112A - Petya and Strings	Python 3	Accepted	92 ms	0 KB
+s1=input().lower()
+s2=input().lower()
+if(s1==s2) :
+    print(0)
+elif(s1>s2) :
+    print(1)
+elif(s1<s2) :
+    print(-1)
